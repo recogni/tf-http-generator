9bb3063b779e0df13fc3e0f1536c5eb834eb65b2
Berend Ozceri <berend@recogni.com>
2019-10-29 11:47:06 +0100

    Update version number.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name="tf_http_generator",
-    version="0.2.1",
+    version="0.2.2",
     description="Tensorflow data generator, sourced from HTTP POSTs",
     url="https://github.com/recogni/tf-http-generator",
     author="sabhiram",
